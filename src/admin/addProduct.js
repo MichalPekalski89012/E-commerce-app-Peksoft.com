9d18a9731257268373afdec614a9b917d4906bca
MichalPekalski89012 <michalpekalski890@gmail.com>
2023-11-03 14:02:23 +0100

    parameter table for adding product

--- src/admin/addProduct.js
+++ src/admin/addProduct.js
@@ -3,11 +3,12 @@ import { productsColRef,addDoc} from "../index.js";
 
 const categoryList = document.querySelector('.js-category-list');
 const subcategoryList = document.querySelector('.js-subcategory-list');
+const parametersTable = document.querySelector('.js-parameters-table');
 let categoryValue = '';
-
+let parametersTableHtml = '';
 categoryList.addEventListener('change',()=>{
   categoryValue = categoryList.value;
-  //console.log(`to jest kategoria:${categoryValue}`);
+  
   changeSubcategory(categoryValue);
 
 });
@@ -68,15 +69,67 @@ function changeSubcategory(category){
   subcategoryList.innerHTML = subcategoryHtml;
 }
 
+subcategoryList.addEventListener('change',()=>{
+  console.log(subcategoryList.value);
+  switchParemetersTable(subcategoryList.value);
+});
 
 
 
-
-
-
-
-
-
+function switchParemetersTable(subcategory){
+  switch(subcategory){
+    case 'playstation':
+    case 'xbox':
+    case 'switch':
+    case 'otherConsoles':
+      parametersTableHtml = `<tr>
+      <th>Parametr</th>
+      <th>Wartość</th>
+    </tr>
+    <tr>
+      <td>Procesor</td>
+      <td><input type="text" name="parameter1"></td>
+    </tr>
+    <tr>
+      <td>Układ graficzny</td>
+      <td><input type="text" name="parameter2"></td>
+    </tr>
+    <tr>
+      <td>Pamięć RAM</td>
+      <td><input type="text" name="parameter3"></td>
+    </tr>
+    <tr>
+      <td>Dysk</td>
+      <td><input type="text" name="parameter4"></td>
+    </tr>
+    <tr>
+      <td>Łączność</td>
+      <td><input type="text" name="parameter5"></td>
+    </tr>
+    <tr>
+      <td>Napęd optyczny</td>
+      <td><input type="text" name="parameter6"></td>
+    </tr>
+    <tr>
+      <td>Złącza</td>
+      <td><input type="text" name="parameter7"></td>
+    </tr>
+    <tr>
+      <td>kolor</td>
+      <td><input type="text" name="parameter8"></td>
+    </tr>
+    <tr>
+      <td>Dodatkowe informacje</td>
+      <td><input type="text" name="parameter9"></td>
+    </tr>
+    <tr>
+      <td>Dołączone akcesoria</td>
+      <td><input type="text" name="parameter10"></td>
+    </tr>`;
+      break;
+  }
+  parametersTable.innerHTML = parametersTableHtml;
+}
 
 
 const addProductForm = document.querySelector('.js-add-product-form');
@@ -88,7 +141,19 @@ addProductForm.addEventListener('submit',(e)=>{
     price: addProductForm.price.value,
     category: addProductForm.category.value,
     subcategory: addProductForm.subcategory.value,
-    description: addProductForm.description.value
+    description: addProductForm.description.value,
+    parameters: {
+      parameter1: addProductForm.parameter1.value,
+      parameter2: addProductForm.parameter2.value,
+      parameter3: addProductForm.parameter3.value,
+      parameter4: addProductForm.parameter4.value,
+      parameter5: addProductForm.parameter5.value,
+      parameter6: addProductForm.parameter6.value,
+      parameter7: addProductForm.parameter7.value,
+      parameter8: addProductForm.parameter8.value,
+      parameter9: addProductForm.parameter9.value,
+      parameter10: addProductForm.parameter10.value
+    }
   }).then(()=>{
     addProductForm.reset();
   });
