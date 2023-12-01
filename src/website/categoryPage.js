01b8e82ecf1c9ae87a44b514fa44488071e88da2
MichalPekalski89012 <michalpekalski890@gmail.com>
2023-12-01 13:32:47 +0100

    listing products test

--- src/website/categoryPage.js
+++ src/website/categoryPage.js
@@ -1,3 +1,11 @@
 // kod generujący zawartość strony kategori. Bedzie generować m.in.
 // polecane w danej kategorii
 // listę podkategorii
+export let subcategory = localStorage.getItem('subcategoryValue');
+
+document.querySelectorAll(".subcategory-redirect").forEach((link)=>{
+  link.addEventListener("click", ()=>{
+    subcategory = link.dataset.subcategory;
+    localStorage.setItem('subcategoryValue', subcategory);
+  });
+});
--- src/website/subcategoryPage.js
+++ src/website/subcategoryPage.js
@@ -1,1 +1,17 @@
 // podstrona generująca listę produktów w zależnosći od tego jaką kategorię wybrał użytkownik
+import { subcategory } from "/src/website/categoryPage.js";
+import { doc, onSnapshot, productsColRef, query, where } from "../index.js";
+
+const productList = document.querySelector('.products-list');
+productsListing(subcategory)
+
+function productsListing(subcategory){
+  let q = query(productsColRef,where('subcategory','==',String(subcategory)));
+  onSnapshot(q,(snapshot)=>{
+    snapshot.docs.forEach(doc => {
+      productList.innerHTML += `<div class="product-container">
+      <p>${doc.data().name} ${doc.data().price}</p>
+    </div>`;
+    });
+  });
+}
