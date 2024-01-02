2544e56d966b35824ee634f672c826f70abbe852
MichalPekalski89012 <michalpekalski890@gmail.com>
2024-01-02 15:51:28 +0100

    home page banners test

--- src/website/homePage.js
+++ src/website/homePage.js
@@ -1,1 +1,19 @@
-//kod który będzie generował zawartość strony głównej
+
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+let currentSlide = 0;
+
+function showSlide(index) {
+  const slides = document.querySelector('.slides');
+  const totalSlides = document.querySelectorAll('.slide').length;
+  currentSlide = (index + totalSlides) % totalSlides;
+  slides.style.transform = `translateX(${-currentSlide * 100}%)`;
+  }
+
+prevButton.addEventListener("click",()=>{
+  showSlide(currentSlide - 1);
+});
+
+nextButton.addEventListener("click", () => {
+  showSlide(currentSlide + 1);
+});
